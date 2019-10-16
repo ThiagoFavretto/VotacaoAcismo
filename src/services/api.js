1ad14440d627ba42015cf49e2aa9de172f289914
Thiago Favretto <thiagofavrettosc@gmail.com>
2019-10-16 17:41:06 -0300

    integrando com a api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.102:3333',
+  baseURL: 'http://167.99.155.29:3333',
 });
 
 api.interceptors.request.use(config => {
