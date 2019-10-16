b1c284dee0647bd9e2015eff8edee28cd4a78db4
Thiago Favretto <thiagofavrettosc@gmail.com>
2019-10-16 19:15:52 -0300

    Teste de conexao sem a porta

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://167.99.155.29:3333',
+  baseURL: 'http://167.99.155.29',
 });
 
 api.interceptors.request.use(config => {
