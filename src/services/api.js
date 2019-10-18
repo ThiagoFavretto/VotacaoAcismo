3f8e0339b4d62145861935d503a74844c264bc44
Thiago Favretto <thiagofavrettosc@gmail.com>
2019-10-17 22:34:02 -0300

    Texte conexao

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: '/api/',
+  // [[redirects]]
+  // from = "/vota/*"
+  // to = "https://acad.unoesc.edu.br/:splat"
+  // status = 200
+  // force = true
+  // headers = {X-From = "Netlify"}
 });
 
 api.interceptors.request.use(config => {
