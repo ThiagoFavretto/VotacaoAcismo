d7199852a82be8c01a192abed5b3a90a83a69909
Patrick Battisti <1729patrick@gmail.com>
2019-10-18 17:31:27 +0100

    Tela de podium apenas para usuário administrador

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://destaque2019.acismo.com.br/vota/',
+  // baseURL: 'https://destaque2019.acismo.com.br/vota/',
+  baseURL: 'http://localhost:3333',
 });
 
 api.interceptors.request.use(config => {
