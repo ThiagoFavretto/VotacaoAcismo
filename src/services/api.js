5840f0d28807d01c0de858a9490b8b842f930516
Thiago Favretto <thiagofavrettosc@gmail.com>
2019-10-17 21:06:07 -0300

    Teste conexao

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://monitoramento.mkanet.com.br',
+  baseURL: 'http://167.99.155.29',
 });
 
 api.interceptors.request.use(config => {
