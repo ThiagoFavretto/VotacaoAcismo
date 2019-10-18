7d3cdce2f980b827e3f4cca9401c3d89f6aaffd2
Thiago Favretto <thiagofavrettosc@gmail.com>
2019-10-17 22:44:20 -0300

    Texte conexao

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/',
+  baseURL: '/vota/',
   // [[redirects]]
   // from = "/vota/*"
   // to = "https://acad.unoesc.edu.br/:splat"
